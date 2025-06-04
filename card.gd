1a87a1a633d2a4c048c610a898b3e7ba62d17cd0
Federico Nery <fedegnery@gmail.com>
2025-06-04 15:09:46 -0300

    Added custom font and adjust name and atk hp

--- card.gd
+++ card.gd
@@ -11,8 +11,8 @@ func _ready():
 	var CardSize = Vector2(254, 350)
 	
 	#$Border.scale *= CardSize / $Border.texture.get_size();
-	var baseWidth = $CardImage.texture.get_size().x * 0.355
-	var baseHeight = $CardImage.texture.get_size().y * 0.343
+	var baseWidth = $CardImage.texture.get_size().x * $CardImage.scale.x
+	var baseHeight = $CardImage.texture.get_size().y * $CardImage.scale.y
 	
 	$CardImage.texture = load(CardImg)
 	var imgWidth = $CardImage.texture.get_size().x
@@ -27,25 +27,43 @@ func _ready():
 	
 	#$CardImage.scale = Vector2(240 / $CardImage.texture.get_size().x, 190 / $CardImage.texture.get_size().y)
 	#$CardImage.scale *= CardSize / $CardImage.texture.get_size();
-	var attack = CardInformation.attack
-	var deffense = CardInformation.deffense
+	var attack = str(CardInformation.attack)
+	var deffense = str(CardInformation.deffense)
 	var color = CardInformation.color
 	var name = CardInformation.name
 	
 	print($CardImage.texture.get_size() * $CardImage.scale)
 	
 	$Name.text = str(name)
-	$Atk.text = str(attack)
-	$Hp.text = str(deffense)
+	var labelAtkHp = "{atk}/{hp}"
+	$Atk.text = labelAtkHp.format({"atk": attack, "hp": deffense})
 	
 	var styleBoxFlat: StyleBoxFlat = $Panel.get_theme_stylebox("panel").duplicate()
-	print(styleBoxFlat.border_color)
+	var styleBoxFlatName: StyleBoxFlat = $Name.get_theme_stylebox("normal").duplicate()
+	var styleBoxFlatAtk: StyleBoxFlat = $Atk.get_theme_stylebox("normal").duplicate()
 	if styleBoxFlat is StyleBoxFlat:
 		if color == CardsDatabase.COLORS.get("RED"):
-			styleBoxFlat.border_color = Color(1, 0, 0, 1)  # Rojo
+			styleBoxFlat.border_color = Color.RED
 		if color == CardsDatabase.COLORS.get("BLUE"):
 			styleBoxFlat.border_color = Color(100, 2, 0)  # Rojo
-	print(styleBoxFlat.border_color)
+
+	if styleBoxFlatName is StyleBoxFlat:
+		if color == CardsDatabase.COLORS.get("RED"):
+			styleBoxFlatName.border_color = Color.RED
+		if color == CardsDatabase.COLORS.get("BLUE"):
+			styleBoxFlatName.border_color = Color(100, 2, 0)  # Rojo
+				
+	print(styleBoxFlatAtk.bg_color)
+	if styleBoxFlatAtk is StyleBoxFlat:
+		if color == CardsDatabase.COLORS.get("RED"):
+			styleBoxFlatAtk.bg_color = Color.DARK_RED
+	print(styleBoxFlatAtk.bg_color)
+	
+	$Name.remove_theme_stylebox_override("normal")
+	$Name.add_theme_stylebox_override("normal", styleBoxFlatName)
+	
+	$Atk.remove_theme_stylebox_override("normal")
+	$Atk.add_theme_stylebox_override("normal", styleBoxFlatAtk)
 	
 	$Panel.remove_theme_stylebox_override("panel")
 	$Panel.add_theme_stylebox_override("panel", styleBoxFlat)
@@ -56,16 +74,8 @@ func _ready():
 
 
 func _draw():
-	var rect = Rect2(Vector2(50, 50), Vector2(200, 229)) # Define el área del rectángulo
-	var radius = 20 # Radio de las esquinas redondeadas
-	var style = StyleBoxFlat.new()
-	style.bg_color = Color(255, 255, 255, 0)
+	pass
 
-	draw_style_box(style, rect) # Dibuja el fondo
-	draw_arc(Vector2(rect.position.x + radius, rect.position.y + radius), radius, PI, PI + 0.5 * PI, 10, Color(1, 0, 0)) # Esquina superior izquierda
-	draw_arc(Vector2(rect.position.x + rect.size.x - radius, rect.position.y + radius), radius, PI + 0.5 * PI, 2* PI, 10, Color(1, 0, 0)) # Esquina superior derecha
-	draw_arc(Vector2(rect.position.x + radius, rect.position.y + rect.size.y - radius), radius, PI, PI/2, 10, Color(1, 0, 0)) # Esquina inferior izquierda
-	draw_arc(Vector2(rect.position.x + rect.size.x - radius, rect.position.y + rect.size.y - radius), radius, 2*PI, 2*PI+0.5*PI, 10, Color(1, 0, 0)) # Esquina inferior derecha
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
